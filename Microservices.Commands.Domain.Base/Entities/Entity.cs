b3a86d6b899bd7d093462c6b085bfc2a186b0011
Carlos Dias <carlos@gohiper.com.br>
2020-05-23 10:58:15 -0300

    Add project files.

---
+++ Microservices.Commands.Domain.Base/Entities/Entity.cs
@@ -0,0 +1,37 @@
+﻿using Microservices.Commands.Domain.Base.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Microservices.Commands.Domain.Base.Entities
+{
+    public class Entity : BaseValidation, IBaseValidation
+    {
+        protected Entity(Guid id)
+        {
+            SetId(id);
+        }
+
+        protected Entity()
+        {
+            SetId(Guid.NewGuid());
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Indice { get; set; }
+
+        private void SetId(Guid id)
+        {
+            if (id == default)
+            {
+                AddError($"Não é possível inserir no Id o valor: {id}");
+                return;
+            }
+
+            Id = id;
+        }
+    }
+}
---
+++ Microservices.Commands.Domain.Base/Validation/IBaseValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Microservices.Commands.Domain.Base.Validation
+{
+    public interface IBaseValidation
+    {
+        void AddError(IEnumerable<string> erros);
+
+        void AddError(string erro);
+
+        void AddError(BaseValidation baseValidation);
+
+        public void AddError(IEnumerable<BaseValidation> baseValidation);
+
+        ICollection<string> GetErrors();
+
+        bool IsValid();
+    }
+}
